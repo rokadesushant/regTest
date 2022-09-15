3067c78571568e59799ace0475e732bae8b1ec73
SUSHANT ROKADE <42928454+rokadesushant@users.noreply.github.com>
2022-09-15 20:06:15 +0530

    Update webhookprocess.py

--- webhookprocess.py
+++ webhookprocess.py
@@ -41,11 +41,11 @@ def mdrmcsv():
         print(out_file)
        
         # extracting required file from zipfile
-        
+        '''
         with zipfile.ZipFile(file_name) as zf:
             
             #zf.extract('MDRM_CSV.csv')
-'''
+
     # deleting the zipfile from the directory
     os.remove('MDRM.zip')
 
