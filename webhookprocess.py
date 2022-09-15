0a6c03eb26e6420f24a98ef55a6d4aa6f7a354bf
SUSHANT ROKADE <42928454+rokadesushant@users.noreply.github.com>
2022-09-15 21:07:58 +0530

    Update webhookprocess.py

--- webhookprocess.py
+++ webhookprocess.py
@@ -24,65 +24,65 @@ def favicon():
 @app.route('/homepage')
 def home():
     print('success abc')
-    app.logger.info('success 1')
     return "Hello World Success changed"
 
 @app.route('/mdrmcsv')
 def mdrmcsv():
+    
     url = 'https://www.federalreserve.gov/apps/mdrm/pdf/MDRM.zip'
 
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 
-#resp = urllib.request.urlopen(url)
+    #resp = urllib.request.urlopen(url)
 
-zipfile = ZipFile(BytesIO(urlopen(req).read()))
+    zipfile = ZipFile(BytesIO(urlopen(req).read()))
 
-csvdata = TextIOWrapper(zipfile.open('MDRM_CSV.csv'),encoding='utf-8')
+    csvdata = TextIOWrapper(zipfile.open('MDRM_CSV.csv'),encoding='utf-8')
 
-data = pd.read_csv(csvdata,skiprows=1)
+    data = pd.read_csv(csvdata,skiprows=1)
 
-#print(data)
+    #print(data)
 
-data["Name"] = ""
+    data["Name"] = ""
 
-res = data.columns
-print(res)
+    res = data.columns
+    print(res)
 
-data.columns.values[0] = "Mnemonic__c"
-data.columns.values[1] = "Item_Code__c"
-data.columns.values[2] = "Start_Date__c"
-data.columns.values[3] = "End_Date__c"
-data.columns.values[5] = "Confidentiality__c"
-data.columns.values[6] = "Item_Type__c"
-data.columns.values[7] = "Reporting_form__c"
-
-
-
-mdrmDataDict = data.to_dict('records')
-
-count=0
-
-# format the data
-for mdrm in mdrmDataDict:
-
-    if mdrm['Name'] =='':
-        # print('reporting form',type(mdrm['Reporting_form__c']))
-        # print('Mnomonic',type(mdrm['Mnemonic__c']))
-        # print('Item code',type(mdrm['Item_Code__c']))
-        # print('Name',type(mdrm['Name']))
-        mdrm['Name']= str(mdrm['Reporting_form__c'])+mdrm['Mnemonic__c']+str(mdrm['Item_Code__c'])
-
-    if mdrm['Description']:
-        mdrm['Description'] = re.sub('<[^<]+?>', '', str(mdrm['Description']))
-        mdrm['Description'] = mdrm['Description'].replace('&#x0D;','')
-        count += 1
-        #print(mdrm['Description'])
+    data.columns.values[0] = "Mnemonic__c"
+    data.columns.values[1] = "Item_Code__c"
+    data.columns.values[2] = "Start_Date__c"
+    data.columns.values[3] = "End_Date__c"
+    data.columns.values[5] = "Confidentiality__c"
+    data.columns.values[6] = "Item_Type__c"
+    data.columns.values[7] = "Reporting_form__c"
 
 
-#print(type(d))
 
-print(count)
-print(mdrmDataDict[1])
+    mdrmDataDict = data.to_dict('records')
+
+    count=0
+
+    # format the data
+    for mdrm in mdrmDataDict:
+
+        if mdrm['Name'] =='':
+            # print('reporting form',type(mdrm['Reporting_form__c']))
+            # print('Mnomonic',type(mdrm['Mnemonic__c']))
+            # print('Item code',type(mdrm['Item_Code__c']))
+            # print('Name',type(mdrm['Name']))
+            mdrm['Name']= str(mdrm['Reporting_form__c'])+mdrm['Mnemonic__c']+str(mdrm['Item_Code__c'])
+
+        if mdrm['Description']:
+            mdrm['Description'] = re.sub('<[^<]+?>', '', str(mdrm['Description']))
+            mdrm['Description'] = mdrm['Description'].replace('&#x0D;','')
+            count += 1
+            #print(mdrm['Description'])
+
+
+    #print(type(d))
+
+    print(count)
+    print(mdrmDataDict[1])
     return "MDRMCSV Success"
 
 
